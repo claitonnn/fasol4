e537b15645eedbc1620e27c964103d9907ee7118
Your Name <nandosousa@gmail.com>
2012-11-13 19:34:21 -0300

    criando minha primeira aplicacao rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fasol::Application.config.secret_token = '342c0da1a95ee3e45e84727e434ab1c9cf512dcc9fdf7e7a89e53ccf4f3ca6438ba39d0ee34da50fe9b2162f8db4c7ee760b1ecb93cf65f994d519cc202457f7'
